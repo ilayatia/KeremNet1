5aa48bd18613c55625200e8ce0354942103815d8
ilay atia <ilayatia@gmail.com>
2025-06-24 13:07:52 +0300

    doone

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <RecipeReviewCard likes={66} comments={[{name:"master shifu",text:"i agree"},{name:"po",text:"im hungry"}]} name="Ilay" date="127.0.0.1" text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum' />
+       <RecipeReviewCard likes={66} comments={[{name:"master shifu",text:"i agree"},{name:"po",text:"im hungry"}]} name="Ilay" date="22/09/2004 12:12" text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum' />
       </header>
     </div>
   );
