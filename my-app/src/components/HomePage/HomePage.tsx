87f4b6fcdf44576b136f346e2e6ac4059fe0d416
ilay atia <ilayatia@gmail.com>
2025-06-25 09:11:58 +0300

    using post object instead od destructing it

--- my-app/src/components/HomePage/HomePage.tsx
+++ my-app/src/components/HomePage/HomePage.tsx
@@ -10,7 +10,7 @@ export default function HomePage(){
        {posts.map((post,index)=>{
         return <div key={index}>
         <div  className="margin"></div>
-        <Post  post={{comments:post.comments,date:post.date,likes:post.likes,name:post.name,text:post.text}}></Post></div>
+        <Post  post={post}></Post></div>
        })}
     
     </>
