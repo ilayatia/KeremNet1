ddee00888b676aa370db4f8ec0150def3e5aa610
ilay atia <ilayatia@gmail.com>
2025-06-25 12:12:59 +0300

    added nice card

--- my-app/src/components/applicationLayout/Header/Header.tsx
+++ my-app/src/components/applicationLayout/Header/Header.tsx
@@ -1,7 +1,13 @@
 import Navbar from "../NavBar/Navbar";
-import './Header.css'
-export default function Header(){
-    return <header className="header">
-            <h1>KeremNet</h1>
-        </header>
+import "./Header.css";
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="card">
+        <span></span>
+        <div className="content">KeremNet</div>
+      </div>
+
+    </header>
+  );
 }
