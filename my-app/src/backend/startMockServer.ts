5362f9bff7ce6bd16f687efa4f8961800bf5c7c7
ilay atia <ilayatia@gmail.com>
2025-06-26 15:01:14 +0300

    deleted mirage code

--- my-app/src/backend/startMockServer.ts
+++
@@ -1,9 +0,0 @@
-import { createServer } from "miragejs";
-import data from './data.json'
-export function StartServer(){
-    createServer({
-  routes() {
-    this.get("/api/posts", () => data.posts);
-  },
-});
-}
