8c59ab0411f1f9401e39553812ec7deca6774874
ilay atia <ilayatia@gmail.com>
2025-06-26 15:02:18 +0300

    deleted mirage code

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,11 +5,8 @@ import Comment from "./components/Comment/Comment";
 import Post from "./components/Post/Post";
 import HomePage from "./components/HomePage/HomePage";
 import { createServer } from "miragejs";
-import data from "./backend/data.json";
-import { StartServer } from "./backend/startMockServer";
 import Layout from "./components/applicationLayout/Layout/Layout";
 
-StartServer()
 
 function App() {
   return (
