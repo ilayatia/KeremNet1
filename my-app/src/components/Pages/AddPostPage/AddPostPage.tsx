c6d4c9e46cb6a58be092c91053e08da2ddc7cc5f
ilay atia <ilayatia@gmail.com>
2025-06-27 09:44:09 +0300

    added forms and styling

--- my-app/src/components/Pages/AddPostPage/AddPostPage.tsx
+++ my-app/src/components/Pages/AddPostPage/AddPostPage.tsx
@@ -5,15 +5,45 @@ import useJson from "../../Hooks/use-json";
 import { getPosts } from "../../../backend/api";
 import "./AddPostPage.css";
 
+interface PostToPostToServer{
+    date:string,
+    likes:number,
+    name:string,
+    text:string
+}
 export default function AddPostPage() {
-    const [formData,setFormData] = useState<PostModel>()
+    const [formData,setFormData] = useState<PostToPostToServer>({date:"",likes:0,name:"",text:""})
+    function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  }
+  function onSubmit(){
+    //post formdata To server
+}
   return (
-    <div>
-      <form>
+    <div className="div-border">
+      <form onSubmit={onSubmit}>
         <label>
             Name:
-            <input type="text" name="name" value={formData?.name}></input>
+            <input type="text" name="name" value={formData?.name} onChange={handleInputChange}></input>
         </label>
+        <br />
+        <label>
+            Date:
+            <input type="text" name="date" value={formData?.date} onChange={handleInputChange}></input>
+        </label>
+        <br />
+        <label>
+            Text:
+            <input type="text" name="text" value={formData?.text} onChange={handleInputChange}></input>
+        </label>
+        <br />
+        <label>
+            Likes:
+            <input type="number" name="likes" value={formData?.likes} onChange={handleInputChange}></input>
+        </label>
+        <br />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
